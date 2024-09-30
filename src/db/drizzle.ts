849bac7b2cc1dd741c64e79f4502cb7fd732a646
João Vitor <jv.zaniolo@gmail.com>
2024-09-30 20:22:31 -0300

    misc

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,11 +1,9 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { createPool } from '@vercel/postgres'
+import { drizzle } from 'drizzle-orm/vercel-postgres'
 import * as schema from './schema'
 
-if (!process.env.DATABASE_URL) {
-	throw new Error('DATABASE_URL is not set')
-}
+export const sql = createPool({
+	connectionString: process.env.POSTGRES_URL,
+})
 
-export const client = postgres(process.env.DATABASE_URL)
-
-export const db = drizzle(client, { schema, logger: process.env.NODE_ENV === 'development' })
+export const db = drizzle(sql, { schema, logger: process.env.NODE_ENV === 'development' })
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,13 +1,6 @@
 import dayjs from 'dayjs'
-import postgres from 'postgres'
-import { db } from './drizzle'
+import { db, sql } from './drizzle'
 import { goalCompletions, goals } from './schema'
-
-if (!process.env.DATABASE_URL) {
-	throw new Error('DATABASE_URL is not set')
-}
-
-const client = postgres(process.env.DATABASE_URL)
 
 async function seed() {
 	await db.delete(goalCompletions)
@@ -35,5 +28,5 @@ seed()
 		console.log('🌱 Database seeded successfully!')
 	})
 	.finally(() => {
-		client.end()
+		sql.end()
 	})
