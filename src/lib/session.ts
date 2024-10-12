37eb191f171770b7f17ada82a8c39e66d13bbfbc
João Vitor <jv.zaniolo@gmail.com>
2024-10-12 13:00:24 -0300

    misc

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,8 +1,5 @@
 import 'server-only'
 import { SignJWT, jwtVerify } from 'jose'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { cache } from 'react'
 
 type SessionPayload = {
 	userId: string
@@ -14,6 +11,7 @@ const encodedKey = new TextEncoder().encode(secretKey)
 
 export async function encrypt(payload: SessionPayload) {
 	return new SignJWT(payload)
+		.setProtectedHeader({ alg: 'HS256' })
 		.setIssuedAt()
 		.setExpirationTime('7d')
 		.sign(encodedKey)
@@ -21,7 +19,9 @@ export async function encrypt(payload: SessionPayload) {
 
 export async function decrypt(session: string | undefined = '') {
 	try {
-		const { payload } = await jwtVerify(session, encodedKey)
+		const { payload } = await jwtVerify(session, encodedKey, {
+			algorithms: ['HS256'],
+		})
 		return payload
 	} catch (error) {
 		console.log('Failed to verify session')
